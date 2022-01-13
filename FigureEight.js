62693698dacc13fbe2ee64254d3e21a69f6dc1b1
Spencer <spencerkohler52507@gmail.com>
2022-01-13 09:32:33 -0800

    First part functional

---
+++ FigureEight.js
@@ -0,0 +1,10 @@
+/*
+Programmer: Spencer Kohler
+Date: 1-13-2022
+Program: Here I am experimenting to see what the sphero bolt does
+*/
+
+async function startProgram() {
+	// Write code here
+	await roll(35, 150, 1);
+}
