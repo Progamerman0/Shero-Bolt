7f8633e161723065ab93e70aaa72d30ae7592ba6
Spencer <spencerkohler52507@gmail.com>
2022-01-17 09:48:50 -0800

    Text sent successfully

--- MartinLutherKing/MLK.js
+++ MartinLutherKing/MLK.js
@@ -6,6 +6,11 @@ Program: Martin Luther King
 */
 async function startProgram() {
 	// Write code here
-	await speak("I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.");
+	//await speak("I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.");
+	await scrollMatrixText('I am happy to join with', {r: 256, g: 256, b:256}, 10, true);
+	await scrollMatrixText(' you today in what will', {r: 256, g: 256, b:256}, 10, true);
+	await scrollMatrixText('go down in history as the', {r: 256, g: 256, b:256}, 10, true);
+	await scrollMatrixText('greatest demonstration for freedom in the history of our nation.', {r: 256, g: 256, b:256}, 10, true);
 }
+
 
