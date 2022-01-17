52c2d7a206272b2ca3ffaa830c1e95e4889d5d03
Spencer <spencerkohler52507@gmail.com>
2022-01-17 09:29:36 -0800

    First sentence works

---
+++ MartinLutherKing/MLK.js
@@ -0,0 +1,11 @@
+/*
+Programer: Spencer Kohler
+Team Member: 
+Date: 1-17-2022
+Program: Martin Luther King
+*/
+async function startProgram() {
+	// Write code here
+	await speak("I am happy to join with you today in what will go down in history as the greatest demonstration for freedom in the history of our nation.");
+}
+
