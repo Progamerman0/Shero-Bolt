c37b925d22978c8c023e011f1289502ef1487555
Spencer <spencerkohler52507@gmail.com>
2022-01-26 09:30:38 -0800

    Intro to Haunted Mansion program

---
+++ HsuntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -0,0 +1,9 @@
+/*
+Programmer: Spencer Kohler
+Team Member: Johana
+Date: 1-26-2022
+Program: We will make a program to navigate a maze with particular challanges at particular points
+with a sphero bolt.
+*/
+
+
