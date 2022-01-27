2b630febf966ef4a26f50c664ad00199d93122b6
Spencer <spencerkohler52507@gmail.com>
2022-01-27 09:50:17 -0800

    Josiah Blue Light Successful

--- HsuntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HsuntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -6,4 +6,12 @@ Program: We will make a program to navigate a maze with particular challanges at
 with a sphero bolt.
 */
 
+async function startProgram() {
+	await goBlueLight();
+}
 
+async function goBlueLight(){
+	await scrollMatrixText('Start' {r:255, g:0, b:0}, 30, true)
+	await roll(0, 100, 1.45);
+	await setMainLed({r:0, g:0, b:255});
+}
