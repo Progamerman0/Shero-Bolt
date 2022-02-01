3b17232e09494eed7ccf24bfee8371783274e592
Spencer <spencerkohler52507@gmail.com>
2022-02-01 10:03:14 -0800

    Green Light progress

--- HsuntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HsuntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -1,33 +1,50 @@
 /*
-Programmer: Spencer Kohler
-Team Member: Johana
-Date: 1-26-2022
-Program: We will make a program to navigate a maze with particular challanges at particular points
-with a sphero bolt.
+Programmer: Josiah Fuller
+Team Member: Spencer
+Date: 1.26.2022
+Program: We will be making a program to go through a haunted mansion
+ filled with haunted sounds, spooky color changes, and other obstacles
 */
 
-async function startProgram(){ //This makes it do the other functions.
-	await goBlueLight();
+async function startProgram(){
+	await goToBlueLight();
 	await goSound1();
+	await goRedLight();
+	await greenLight();
 }
 
-async function goBlueLight(){ //This function makes the bolt say start and makes it go to the blue circle and turn blue.
-	await scrollMatrixText('Start' {r:255, g:0, b:0}, 30, true)
-	await roll(0, 100, 1.45);
-	await setMainLed({r:0, g:0, b:255});
+async function goToBlueLight(){
+	await scrollMatrixText('Start', {r: 255, g: 0, b: 0}, 30, true);
+	await roll(0, 50, 3.1);
+	await setMainLed({ r: 0, g: 0, b: 255 });
 }
 
-async function goSound1(){ //This makes it go to the first sound and play an ambulence sound.
+async function goSound1(){
 	await delay(0.5);
-	await roll(90, 100, 1.1);
-	await Sound.Mechanical.Ambulance.play(true);
+	await roll(90, 50, 2.1);
+	await Sound.Mechanical.Ambulance.play(false);
 }
 
-async function goRedLight(){ //This makes the bolt go to the red spot and turn the lights red.
+async function goRedLight(){
 	await delay(0.5);
 	await roll(180, 50, 1.5);
 	await roll(220, 50, 1.3);
-	await delay(0.5);
-	await roll(130, 50, 0.9);
+	await delay(.5);
+	await roll(130, 50, .9);
 	await setMainLed({ r: 255, g: 0, b: 0 });
+	await delay(0.5)
+	
+}
+
+async function greenLight(){
+	await roll(40, 50, 1.3);
+	await Sound.Mechanical.Alarm.play(false);
+	await delay(0.5);
+	await roll(130, 50, 1);
+	await delay(0.5);
+	await roll(185, 50, .9);
+	await roll(95, 50, .6);
+	await delay(0.5);
+	await setMainLed({ r: 0, g: 255, b: 0 });	
+	
 }
